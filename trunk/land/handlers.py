a44b764fafb75b8b26406293d467b7e41c8e438d
vsirisanthana@gmail.com <vsirisanthana@gmail.com@35f8aedc-f5d1-93bd-afce-97ff41cc2104>
2012-05-25 08:27:48 +0000

    - Require login to use app

--- trunk/land/handlers.py
+++ trunk/land/handlers.py
@@ -7,6 +7,16 @@ from .models import Land
 from .utils import to_dict
 
 
+import jinja2
+import os
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    variable_start_string='{$',
+    variable_end_string='$}',
+)
+
+
 class LandInstanceHandler(webapp2.RequestHandler):
 
 #    def __init__(self, resource):
@@ -58,12 +68,29 @@ class LandInstanceHandler(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
 
 
+from google.appengine.api import users
+#def login_required(method):
+#    user = users.get_current_user()
+#    if user:
+#        return method
+#    else:
+#        return False
+
+from webapp2_extras.appengine.users import login_required
+
+#from google.appengine.ext.webapp.util import login_required
+
 class LandListOrCreateHandler(webapp2.RequestHandler):
 
+#    @login_required
     def get(self):
         lands = [to_dict(land) for land in Land.gql('ORDER BY last_modified DESC')]
         self.response.headers['Content-Type'] = 'application/json'
 #        self.response.out.write(simplejson.dumps({'results': lands}))
+
+        url = users.create_logout_url(self.request.uri)
+#        lands.append(url)
+
         self.response.out.write(simplejson.dumps(lands))
 
     def post(self):
@@ -85,3 +112,24 @@ class LandListOrCreateHandler(webapp2.RequestHandler):
         self.response.set_status(201)
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(simplejson.dumps(to_dict(land)))
+
+
+class MainHandler(webapp2.RequestHandler):
+
+    @login_required
+    def get(self):
+        user = users.get_current_user()
+        logout_url = users.create_logout_url(self.request.uri)
+
+        template = jinja_environment.get_template('index.html')
+        self.response.out.write(template.render({
+            'nickname': user.nickname(),
+            'logout_url': logout_url,
+        }))
+
+class TemplateHandler(webapp2.RequestHandler):
+
+    @login_required
+    def get(self, name):
+        template = jinja_environment.get_template(name)
+        self.response.out.write(template.render())
